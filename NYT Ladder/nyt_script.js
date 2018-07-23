e2d744444e704265a6e7af2cc6756fcba38468c2
lfvg <lfvg@cin.ufpe.br>
2018-07-23 10:54:45 -0300

    Commit dos primeiros arquivos
    Arquivos ainda só testam a viabilidade da ideia.

---
+++ NYT Ladder/nyt_script.js
@@ -0,0 +1,7 @@
+var paragrafos = document.getElementsByClassName("css-1i0edl6 e2kc3sl0");
+if(paragrafos.length!=0){
+	alert(paragrafos[0].innerText);
+}
+else{
+	alert("Paragrafos: " + paragrafos.length);
+}
