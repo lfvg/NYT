73d6db980d426a1845218292ebdb45e67643c4cf
Felipe Véras <lfvg@cin.ufpe.br>
2019-06-01 23:11:51 -0300

    Adaptação ao novo comportamento do site
    Essa modificação deve contornar a nova mudança de comportamento do site ao salvar a materia logo apos a pagina ser carregada, e antes do script do NY Times editar o DOM.
    Ainda são precisos mais alguns testes para confirmar o funcionamento correto da extensão.

--- NYT Ladder/content_scripts/nyt_script.js
+++ NYT Ladder/content_scripts/nyt_script.js
@@ -1,3 +1,27 @@
+var documentCopy= document.cloneNode(true);// = wrappedJSObject.document.cloneNode(true);
+//body.onload = function(){
+//	alert("body");
+//};
+//document.addEventListener("DOMContentLoaded", function(event) {
+//    console.log("Dom carregado");
+	//documentCopy = document.cloneNode(true);
+//});
+//window.onload = function() {
+ // documentCopy = document.cloneNode(true);
+//};
+/*
+document.addEventListener('DOMContentLoaded', onInit, false);
+
+function onInit(){
+	/*documentCopy = document.cloneNode(true);
+	var artigo = [];
+	var articleBody = documentCopy.querySelector("section[name=articleBody]");
+	var articleContent = articleBody.querySelectorAll("p");
+	artigo.push(articleContent[i].innerText);
+	alert("teste");
+		
+}*/
+
 //ADICIONA UM LISSENER PARA RECEBER MENSAGENS DO POPUP
 browser.runtime.onMessage.addListener((message) => {
 	//DECLARAÇÃO DE VARIAVEIS QUE VAO SALVAR O TITULO(STRING), SUBTITULO(STRING) - SE EXISTIR
@@ -11,15 +35,15 @@ browser.runtime.onMessage.addListener((message) => {
 	//PROCEDE PARA O SEGUINTE , CASO NAO IGNORA 
 	if (message.command === "getNYTArticle"){
 		//PROCURA AS TAGS PELO SUA CLASSNAME - LEMBRANDO QUE TITULO PODE SER VAZIO
-		var tempTitle = document.getElementsByClassName("balancedHeadline");
-		var tempSubTitle = document.getElementsByClassName("css-p2vh5c ewc5vgb0");
-		var autores = document.getElementsByClassName("css-1cbhw1y e1x1pwtg1");
+		var tempTitle = documentCopy.getElementsByClassName("balancedHeadline");
+		var tempSubTitle = documentCopy.getElementsByClassName("css-p2vh5c ewc5vgb0");
+		var autores = documentCopy.getElementsByClassName("css-1cbhw1y e1x1pwtg1");
 		//DE MODO A EVITAR UM NULL POINT VERIFICASSE PRIMEIRO SE OS ELEMENTOS NAO SAO VAZIOS
 		if (tempTitle.length!=0) titulo = tempTitle[0].innerText;
 		if(tempSubTitle.length != 0) subtitulo = tempSubTitle[0].innerText;
 		//esse select funciona :)
-		var articleBody = document.querySelector("section[name=articleBody]");
-		var articleContent = articleBody.querySelectorAll("p, figure")
+		var articleBody = documentCopy.querySelector("section[name=articleBody]");
+		var articleContent = articleBody.querySelectorAll("p, figure");
 		
 		for(var i = 0; i<articleContent.length; i++){
 			if(articleContent[i].nodeName ==="P"){
